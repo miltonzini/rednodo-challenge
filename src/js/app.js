1828d46ac581c92fc74467d27b0e16fa5782fa8e
miltonzini <miltonzini@gmail.com>
2024-02-23 16:09:48 -0300

    update navbar menu functionality
    applyfunctionality to "menu" anchor

--- src/js/app.js
+++ src/js/app.js
@@ -1,55 +1,44 @@
-const btnHamburguer = document.querySelector('#btnHamburguer');const body = document.querySelector('body');
+const btnHamburguer = document.querySelector('#btnHamburguer');
+const menuLink = document.querySelector('#menuLink');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 const headerAnchor = document.querySelectorAll('.header__menu__anchor');
 
+function toggleMenu() {
+  if (header.classList.contains('open')) { // close Hamburger menu 
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
 
-btnHamburguer.addEventListener('click', function(){
-    console.log('open haburguer');
+  } else { // Open Hamburger menu  
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+  }
+}
 
-    if (header.classList.contains('open')){ // close Hamburger menu 
-        body.classList.remove('noscroll');
-        header.classList.remove('open');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');    
-        });
-        
-    }
-    else { // Open Hamburger menu  
-        body.classList.add('noscroll');
-        header.classList.add('open');
-        fadeElems.forEach(function(element){
-            element.classList.remove('fade-out');
-            element.classList.add('fade-in');
-        });    
-    }
+btnHamburguer.addEventListener('click', function () {
+  console.log('open hamburguer');
+  toggleMenu();
+});
+
+menuLink.addEventListener('click', function () {
+  console.log('open menu link');
+  toggleMenu();
 });
 
 // Cerrar Overlay al clickear anchor
 headerAnchor.forEach((anchor) => {
-    anchor.addEventListener('click', () => {
-        console.log("anchor clicked");
-
-        if (header.classList.contains('open')){ // close Hamburger menu 
-            body.classList.remove('noscroll');
-            header.classList.remove('open');
-            fadeElems.forEach(function(element){
-                element.classList.remove('fade-in');
-                element.classList.add('fade-out');    
-            });
-            
-        }
-        else { // Open Hamburger menu  
-            body.classList.add('noscroll');
-            header.classList.add('open');
-            fadeElems.forEach(function(element){
-                element.classList.remove('fade-out');
-                element.classList.add('fade-in');
-            });    
-        }
-    });
+  anchor.addEventListener('click', () => {
+    console.log("anchor clicked");
+    toggleMenu();
+  });
 });
-
-
